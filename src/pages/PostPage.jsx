aa82b2e0860fc29bf57238fc27eb693aec9e8a55
brenker <brenker@theworkshop.com>
2026-07-03 17:01:31 +0200

    Add post page contents

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,11 +1,23 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import DOMPurify from "dompurify";
 
 export function PostPage() {
     const { id } = useParams(); // grabs the ":id" from the URL
+    const [post, setPost] = useState(null)
+    useEffect(() => {
+        fetch(`http://localhost:3000/posts/${id}`)
+        .then(res => res.json())
+        .then(data => setPost(data.post))
+    }, [id])
+
+    if (!post) return <div>Loading ...</div>
+
     return (
         <>
             <h1>Post Page</h1>
-            <p>Post with { id }</p>
+            <h2>{ post.title }</h2>
+            <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content) }} />
         </>
     )
 }
